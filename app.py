9ecf8eb20e02aecac9a20caaa693e2bb00e72f55
Sasha Czerniawski <sashacandoit@gmail.com>
2022-04-10 11:23:20 -0400

    form and details page added and connected

--- app.py
+++ app.py
@@ -18,3 +18,26 @@ def list_users():
 
     users = User.query.all()
     return render_template("list.html", users=users)
+
+
+@app.route('/<user_id>')
+def show_user_detals(user_id):
+    user = User.query.get(user_id)
+    return render_template('details.html', user=user)
+
+
+@app.route('/add-user')
+def show_add_user_form():
+    return render_template("form.html")
+
+
+@app.route('/', methods=["POST"])
+def add_user():
+    first_name = request.form["first_name"] 
+    last_name = request.form["last_name"] 
+    image_url = request.form["image_url"]
+    image_url = image_url if image_url else None
+
+    new_user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+    db.session.add(new_user)
+    db.session.commit()
