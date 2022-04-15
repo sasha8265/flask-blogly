4037da279b49db0188a687fa6717dc3db7fe7454
Sasha Czerniawski <sashacandoit@gmail.com>
2022-04-15 07:25:45 -0400

    delete post handing added

--- app.py
+++ app.py
@@ -119,3 +119,13 @@ def edit_post(post_id):
     db.session.commit()
 
     return redirect(f"/users/{user.id}")
+
+
+@app.route('/posts/<int:post_id>/delete-post', methods=["POST"])
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    user = Post.query.get_or_404(post.user_id)
+    db.session.delete(post)
+    db.session.commit()
+
+    return redirect(f"/users/{user.id}")
